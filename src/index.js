7606841750e1ed5c1d61cd4a919838708f8098ef
ihamza494 <ihamza494@gmail.com>
2021-11-03 19:02:43 +0500

    first-commit React adding component using function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Person(props){
+  return( 
+    <div className='info__tab'>
+            <h3>{props.name}</h3>
+            <p>{props.sent}</p>
+            </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App(){
+  return(
+  <div className='boxes_client'>
+  <Person name='Hamza' sent='Life is test between odds and evens.'/>
+  <Person name='Ali' sent='Life is test between odds and evens.'/>
+  <Person name='Saqib' sent='Life is test between odds and evens.'/>
+  <Person name='Pershotam' sent='Life is test between odds and evens.'/>
+  </div>
+  );
+}
+
+
+
+ReactDOM.render(<App/>,document.getElementById('root'))
+
+
